180677dfaaec81a45360d4def64f0152444fedd3
Tomas Delgado <tdelgadoandrade@frba.utn.edu.ar>
2023-04-30 20:48:01 +0000

    Conexion entre consola y kernel con hilos

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,9 +1,25 @@
 #include "kernel.h"
+typedef struct{
+	 char AX[4];
+	 char BX[4];
+	 char CX[4];
+	 char DX[4];
+	 char EAX[8];
+	 char EBX[8];
+	 char ECX[8];
+	 char EDX[8];
+	 char RAX[16];
+	 char RBX[16];
+	 char RCX[16];
+	 char RDX[16];
+
+}registros_cpu;
+
 typedef struct {
 		int pid;
 		t_list* instrucciones;
 		int program_counter;
-		struct registros_cpu;
+		registros_cpu registros;
 		t_list* tabla_segmentos;
 		int estimado_proxRafaga;
 		int tiempo_llegada_ready;
@@ -20,7 +36,6 @@ int main(void) {
 	char* puerto = config_get_string_value(config, "PUERTO_ESCUCHA");
 
 	int server_fd = iniciar_servidor(puerto);
-
 	log_info(logger, "Servidor listo para recibir al cliente");
 
 	esperar_cliente_hilos(server_fd);
@@ -33,39 +48,6 @@ void iterator(char* value) {
 	log_info(logger,"%s", value);
 }
 
-void esperar_cliente_hilos(int server){
-	while (1) {
-		   pthread_t thread;
-		   int *socket_cliente = malloc(sizeof(int));
-		   *socket_cliente = accept(server, NULL, NULL);
-		   log_info(logger, "Se conecto un cliente!");
-		   recv_handshake(socket_cliente);
-		   pthread_create(&thread,NULL,(void*) atender_cliente,socket_cliente);
-		   pthread_detach(thread);
-		}
 
-}
 
-int atender_cliente(int cliente_fd){
-	t_list lista;
-	while (1) {
-			int cod_op = recibir_operacion(cliente_fd);
-			switch (cod_op) {
-			case MENSAJE:
-				recibir_mensaje(cliente_fd);
-				break;
-			case PAQUETE:
-				lista = recibir_paquete(cliente_fd);
-				log_info(logger, "Me llegaron los siguientes valores:");
-				list_iterate(lista, (void*) iterator);
-				break;
-			case -1:
-				log_error(logger, "el cliente se desconecto. Terminando coneccion");
-				return EXIT_FAILURE;
-			default:
-				log_warning(logger,"Operacion desconocida. No quieras meter la pata");
-				break;
-			}
-		}
 
-}
--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -8,5 +8,40 @@
 #include "server.h"
 
 void iterator(char* value);
+int atender_cliente(int cliente_fd){
+	t_list *lista;
+	while (1) {
+			int cod_op = recibir_operacion(cliente_fd);
+			log_info(logger,"%d",cod_op);
+			switch (cod_op) {
+			case MENSAJE:
+				recibir_mensaje(cliente_fd);
+				break;
+			case PAQUETE:
+				lista = recibir_paquete(cliente_fd);
+				log_info(logger, "Me llegaron los siguientes valores:");
+				list_iterate(lista, (void*) iterator);
+				break;
+			case -1:
+				log_error(logger, "el cliente se desconecto. Terminando conexion");
+				return EXIT_FAILURE;
+			default:
+				log_warning(logger,"Operacion desconocida. No quieras meter la pata");
+				break;
+			}
+		}
 
+}
+void esperar_cliente_hilos(int server){
+	while (1) {
+		   pthread_t thread;
+		   int *socket_cliente = malloc(sizeof(int));
+		   socket_cliente = accept(server, NULL, NULL);
+		   log_info(logger, "Se conecto un cliente!");
+		   recv_handshake(socket_cliente);
+		   pthread_create(&thread,NULL,(void*) atender_cliente,socket_cliente);
+		   pthread_detach(thread);
+		}
+
+}
 #endif /* KERNEL_H_ */
