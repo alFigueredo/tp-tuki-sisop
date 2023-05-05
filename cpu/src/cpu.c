527a201b70519d0af48b9bbb09cbadcadccd4ed0
Tomas Delgado <tdelgadoandrade@frba.utn.edu.ar>
2023-05-05 18:48:55 +0000

    Avance de la CPU

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -1,8 +1,25 @@
 #include "cpu.h"
 
 int main(void) {
+	pcb contexto;
+
 	char* string = string_new();
 	string_append(&string, "!!!Hello CPU!!!");
 	puts(string);
+
+	while(1)
+	{
+		char *instruccion;
+		instruccion = list_get(contexto->program_counter);
+		switch (instruccion)
+		{
+		case "set"{
+
+		}
+		}
+	}
+
+
+
 	return EXIT_SUCCESS;
 }
