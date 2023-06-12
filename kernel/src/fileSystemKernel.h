1a4c59707067bc9899967c2054f290fc996df490
Ian Santiago Ruiz Depetris <iruizdepetris@frba.utn.edu.ar>
2023-06-12 22:39:01 +0000

    Creados los .c y .h, aun no hay nada

---
+++ kernel/src/fileSystemKernel.h
@@ -0,0 +1,24 @@
+#ifndef FILESYSTEMKERNEL_H_
+#define FILESYSTEMKERNEL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <commons/log.h>
+
+typedef struct
+{
+	FILE*
+	int disponible;
+}tablaGlobalDeArchivos;
+
+
+
+
+
+
+
+
+
+
+#endif /* FILESYSTEMKERNEL_H_ */
